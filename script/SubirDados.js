b83fafc27c1067835c2f611b68540a71ce5b499c
gabrielwouDev <gabrielpenhadev@gmail.com>
2026-06-01 19:13:16 -0300

    paginas 2,3 e 4 finalizadas

---
+++ script/SubirDados.js
@@ -0,0 +1,12 @@
+let resposta = document.querySelector('input[name="escolha"]:checked')
+
+let btn = document.querySelector('.avancar')
+
+btn.addEventListener('click', avancar)
+
+function avancar(){
+    let escolha = resposta.value
+
+    let
+}
+
