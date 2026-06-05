acfe45f63ea3fde4d1cf06f2197347b998b46a39
gabrielwouDev <gabrielpenhadev@gmail.com>
2026-06-05 17:07:17 -0300

    alterando img da fase 2

--- script/SubirDados.js
+++ script/SubirDados.js
@@ -5,6 +5,9 @@ btn.addEventListener('click',exibir)
 
 function exibir(){
     let escolha = document.querySelector('input[name="escolha"]:checked')
+    if(escolha === null){
+        alert("Escolha uma alternativa!")
+    }
     let alternativa = escolha.parentElement.querySelector('h2').textContent;
     let resultados = JSON.parse(sessionStorage.getItem('resultados')) || {};
     resultados[nPergunta] = {
