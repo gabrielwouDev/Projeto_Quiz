26e22d0b0234faae33d11c3fbe6330aa5feef16c
gabrielwouDev <gabrielpenhadev@gmail.com>
2026-06-07 16:00:18 -0300

    correção de js

--- script/puxarDados.js
+++ script/puxarDados.js
@@ -1,4 +1,5 @@
 const resultados = JSON.parse(sessionStorage.getItem('resultados'));
+document.getElementById('p1').value = resultados.pergunta1.texto;
 document.getElementById('p2').value = resultados.pergunta2.texto;
 document.getElementById('p3').value = resultados.pergunta3.texto;
 document.getElementById('p4').value = resultados.pergunta4.texto;
